6b3a2ee297bdc18ab4f93ff76d6ca9bc3a51a031
Munuve30 <77729985+Munuve30@users.noreply.github.com>
2021-02-11 11:06:26 +0300

    Add files via upload

---
+++ logical operators.py
@@ -0,0 +1,14 @@
+#logical operators include and or & not
+x=10
+y=6
+print(x and y)
+print(x or y)
+#not is used for reversing answers
+print(not x<y)
+#other uses of logical and relational operators
+#example for and
+s=11
+t=10
+print(y>4 and x<7)
+#example for or
+print(y>4 or x<7)
